e630e61d7b6d2f2ed948354d7153073a7c5f57e6
0xDev <84038223+avocadodefi@users.noreply.github.com>
2024-02-08 13:34:36 +0200

    Update autoisetup.sh

--- autoisetup.sh
+++ autoisetup.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# Update and Upgrade Ubuntu Packages
-sudo apt update && sudo apt upgrade -y
+# Set DEBIAN_FRONTEND to noninteractive to avoid any prompts
+export DEBIAN_FRONTEND=noninteractive
 
-# Install wget
-sudo apt install wget -y
+# Update and Upgrade Ubuntu Packages without any prompts
+sudo apt-get update && sudo apt-get -y upgrade
+
+# Install wget and screen without any prompts
+sudo apt-get install -y wget screen
 
 # Download Go
 wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
 
-# Extract Go and Install Golang
+# Extract Go
 sudo tar -xvf go1.20.2.linux-amd64.tar.gz
-sudo apt install golang -y
 
 # Move Go to /usr/local
 sudo mv go /usr/local
+
+# Install Golang without any prompts
+sudo apt-get install -y golang
 
 # Setting up environment variables
 export GOROOT=/usr/local/go
@@ -27,11 +32,28 @@ echo 'export GOPATH=$HOME/Projects/Proj1' >> $HOME/.profile
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> $HOME/.profile
 
 # Clone the ceremonyclient repository
-git clone https://github.com/quilibriumnetwork/ceremonyclient
+if git clone https://github.com/quilibriumnetwork/ceremonyclient; then
+    echo "Repository cloned successfully."
+else
+    echo "Failed to clone the repository. Exiting."
+    exit 1
+fi
 
-# Starting screen sessions and running commands
-screen -dmS quilibrium bash -c 'cd ceremonyclient/node && GOEXPERIMENT=arenas go run ./...; exec bash'
-screen -dmS quilibrium_wallet bash -c 'cd ceremonyclient/node && GOEXPERIMENT=arenas go run ./... --db-console; exec bash'
+# Starting first screen session, running commands, and then exiting
+screen -dmS quilibrium bash -c '
+    cd ceremonyclient/node
+    GOEXPERIMENT=arenas go run ./...
+    exec bash'
+sleep 5 # Wait for the command to start
+screen -S quilibrium -X stuff $'exit\n'
+
+# Starting second screen session, running commands, and then exiting
+screen -dmS quilibrium_wallet bash -c '
+    cd ceremonyclient/node
+    GOEXPERIMENT=arenas go run ./... --db-console
+    exec bash'
+sleep 5 # Wait for the command to start
+screen -S quilibrium_wallet -X stuff $'exit\n'
 
 # Output keys.yml content (after a delay to allow previous commands to complete)
 sleep 10
