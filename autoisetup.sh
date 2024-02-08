84bb9301830591484fc581ae00ce4972b375dd21
0xDev <84038223+avocadodefi@users.noreply.github.com>
2024-02-08 13:53:08 +0200

    Update autoisetup.sh

--- autoisetup.sh
+++ autoisetup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# Ensure the unattended-upgrades package is installed
+sudo apt-get install unattended-upgrades -y
+
 # Set DEBIAN_FRONTEND to noninteractive to avoid any prompts
 export DEBIAN_FRONTEND=noninteractive
 
+# Enable all automatic updates (optional, if you want all types of updates to be automatic)
+# sudo dpkg-reconfigure --priority=low unattended-upgrades
+
 # Update and Upgrade Ubuntu Packages without any prompts
-sudo apt-get update && sudo apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
+sudo apt-get update
+sudo apt-get upgrade -yq
+sudo apt-get dist-upgrade -yq
 
 # Install wget, screen, and git without any prompts
-sudo apt-get install -y wget screen git
+sudo apt-get install wget screen git -y
 
 # Download Go
 wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
@@ -17,7 +25,7 @@ sudo tar -xvf go1.20.2.linux-amd64.tar.gz
 sudo mv go /usr/local
 
 # Install Golang without any prompts
-sudo apt-get install -y golang
+sudo apt-get install golang -y
 
 # Setting up environment variables
 export GOROOT=/usr/local/go
@@ -29,8 +37,12 @@ echo 'export GOROOT=/usr/local/go' >> $HOME/.profile
 echo 'export GOPATH=$HOME/Projects/Proj1' >> $HOME/.profile
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> $HOME/.profile
 
-# Clone the ceremonyclient repository
-git clone https://github.com/quilibriumnetwork/ceremonyclient
+# Clone the ceremonyclient repository if it does not exist
+if [ ! -d "ceremonyclient" ]; then
+    git clone https://github.com/quilibriumnetwork/ceremonyclient
+else
+    echo "The 'ceremonyclient' directory already exists, skipping cloning."
+fi
 
 # Starting screen sessions and running commands
 screen -dmS quilibrium
