c1adeccef68f4f2b2fc9de7401f9133aebf5e1bd
0xDev <84038223+avocadodefi@users.noreply.github.com>
2024-02-08 13:47:19 +0200

    Update autoisetup.sh

--- autoisetup.sh
+++ autoisetup.sh
@@ -4,7 +4,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Update and Upgrade Ubuntu Packages without any prompts
-sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get update && sudo apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
 
 # Install wget, screen, and git without any prompts
 sudo apt-get install -y wget screen git
@@ -12,10 +12,8 @@ sudo apt-get install -y wget screen git
 # Download Go
 wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
 
-# Extract Go
+# Extract Go and move to /usr/local
 sudo tar -xvf go1.20.2.linux-amd64.tar.gz
-
-# Move Go to /usr/local
 sudo mv go /usr/local
 
 # Install Golang without any prompts
@@ -32,35 +30,34 @@ echo 'export GOPATH=$HOME/Projects/Proj1' >> $HOME/.profile
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> $HOME/.profile
 
 # Clone the ceremonyclient repository
-if git clone https://github.com/quilibriumnetwork/ceremonyclient; then
-    echo "Repository cloned successfully."
-else
-    echo "Failed to clone the repository. Exiting."
-    exit 1
-fi
+git clone https://github.com/quilibriumnetwork/ceremonyclient
 
-# Starting first screen session, running commands, and then exiting
+# Starting screen sessions and running commands
 screen -dmS quilibrium
-screen -S quilibrium -X stuff $'cd ceremonyclient/node\n'
-screen -S quilibrium -X stuff $'GOEXPERIMENT=arenas go run ./...\n'
-sleep 10 # Adjust this sleep time as necessary
-screen -S quilibrium -X stuff $'\003' # Send Ctrl+C to stop the command
-screen -S quilibrium -X stuff $'exit\n'
+sleep 1
+screen -S quilibrium -X stuff 'cd ceremonyclient/node\n'
+sleep 1
+screen -S quilibrium -X stuff 'GOEXPERIMENT=arenas go run ./...\n'
+sleep 5
+screen -S quilibrium -X stuff $'\003'
+sleep 1
+screen -S quilibrium -X stuff 'exit\n'
+sleep 1
 
-# Starting second screen session, running commands, and then exiting
 screen -dmS quilibrium_wallet
-screen -S quilibrium_wallet -X stuff $'cd ceremonyclient/node\n'
-screen -S quilibrium_wallet -X stuff $'GOEXPERIMENT=arenas go run ./... --db-console\n'
-sleep 10 # Adjust this sleep time as necessary
-screen -S quilibrium_wallet -X stuff $'\003' # Send Ctrl+C to stop the command
-screen -S quilibrium_wallet -X stuff $'exit\n'
+sleep 1
+screen -S quilibrium_wallet -X stuff 'cd ceremonyclient/node\n'
+sleep 1
+screen -S quilibrium_wallet -X stuff 'GOEXPERIMENT=arenas go run ./... --db-console\n'
+sleep 5
+screen -S quilibrium_wallet -X stuff $'\003'
+sleep 1
+screen -S quilibrium_wallet -X stuff 'exit\n'
+sleep 1
 
-# Wait for the .config/keys.yml file to be created
-sleep 30 # Adjust this sleep time as necessary
-
-# Output keys.yml content
-if [ -f ceremonyclient/node/.config/keys.yml ]; then
-    cat ceremonyclient/node/.config/keys.yml
+# Check for the existence of the keys.yml file
+if [ -f "ceremonyclient/node/.config/keys.yml" ]; then
+    cat "ceremonyclient/node/.config/keys.yml"
 else
     echo "keys.yml file not found."
 fi
