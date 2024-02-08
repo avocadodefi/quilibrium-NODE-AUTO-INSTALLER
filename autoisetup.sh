1d1b9fa94b094826e4764ff0be2568e886793d97
0xDev <84038223+avocadodefi@users.noreply.github.com>
2024-02-09 00:48:55 +0200

    Update autoisetup.sh

--- autoisetup.sh
+++ autoisetup.sh
@@ -4,19 +4,13 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Update and Upgrade Ubuntu Packages without any prompts
-sudo apt-get update && sudo apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
+sudo apt update && sudo apt -y upgrade
 
 # Install wget, screen, and git without any prompts
-sudo apt-get install -y wget screen git
+sudo apt install -y wget golang git ufw
 
 # Download Go
 wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
-
-# Check if /usr/local/go already exists
-if [ -d "/usr/local/go" ]; then
-    echo "/usr/local/go already exists. Removing existing directory."
-    sudo rm -rf /usr/local/go
-fi
 
 # Extract Go and move to /usr/local
 sudo tar -xvf go1.20.2.linux-amd64.tar.gz
@@ -32,21 +26,14 @@ echo 'export GOROOT=/usr/local/go' >> $HOME/.profile
 echo 'export GOPATH=$HOME/Projects/Proj1' >> $HOME/.profile
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> $HOME/.profile
 
-# Check if the ceremonyclient directory exists and is a Git repository
-if [ -d "ceremonyclient" ]; then
-    if [ -d "ceremonyclient/.git" ]; then
-        echo "The 'ceremonyclient' directory already exists and is a Git repository. Pulling the latest changes."
-        cd ceremonyclient
-        git pull
-        cd ..
-    else
-        echo "The 'ceremonyclient' directory already exists but is not a Git repository. Removing and cloning anew."
-        rm -rf ceremonyclient
-        git clone https://github.com/quilibriumnetwork/ceremonyclient
-    fi
-else
-    git clone https://github.com/quilibriumnetwork/ceremonyclient || { echo "Failed to clone the repository. Exiting."; exit 1; }
-fi
+# Clone the repository
+git clone https://github.com/quilibriumnetwork/ceremonyclient || { echo "Failed to clone the repository. Exiting."; exit 1; }
+
+# Navigate to ceremonyclient/node directory
+cd ceremonyclient/node || { echo "Failed to navigate to ceremonyclient/node directory. Exiting."; exit 1; }
+
+# Run the command
+GOEXPERIMENT=arenas go run ./... --db-console
 
 # Wait for a while to allow initialization
 sleep 30  # Increased from 10 to 30 seconds
